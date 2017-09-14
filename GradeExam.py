9462966de45e688fb3d99b38e3eb2c04aa5b6692
Deodamnatus <jwgsub@gmail.com>
2017-09-13 18:38:49 -0700

    Added SumAndCubesToN

---
+++ GradeExam.py
@@ -0,0 +1,19 @@
+
+
+score = float(input("Score: "))
+
+def grade(score):
+    LetterGrade = ""
+    if score >= 90:
+        LetterGrade = "A"
+    elif score >= 80:
+        LetterGrade = "B"
+    elif score >= 70:
+        LetterGrade = "C"
+    elif score >= 60:
+        LetterGrade = "D"
+    else:
+        LetterGrade = "F"
+    return LetterGrade
+
+print(grade(score))
