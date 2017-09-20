50a570c95107296656c57ea0183ec40e820231ee
Deodamnatus <jwgsub@gmail.com>
2017-09-19 23:55:53 -0700

    Added AreaOfTriangle

---
+++ AreaOfTriangle.py
@@ -0,0 +1,7 @@
+base=int(input("Base: "))
+height=int(input("Height: "))
+
+def getArea(base,height):
+    return base*height
+
+print(getArea(base,height))
--- Testing.py
+++ Testing.py
@@ -1,3 +1,4 @@
+'''
 import math
 
 radius = float(input("Radius:"))
@@ -11,3 +12,9 @@ def sphereVolume(radius):
 	return (4*math.pi*(radius**3))/3
 
 print("Surface Area: ", sphereArea(radius), "\nVolume: ", sphereVolume(radius))
+'''
+
+def squareAdd(a,b,c):
+    return min(min(max(a,b),max(b,c)),min(max(a,c),max(b,c)),min(max(a,b),max(a,c)))**2 + max(a,b,c)**2
+
+print(squareAdd(1,2,3)
