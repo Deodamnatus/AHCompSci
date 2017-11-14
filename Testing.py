8c868bc4ab24ab74b93e4521b755fd3eea2ef9db
Deodamnatus <jwgsub@gmail.com>
2017-11-13 21:05:19 -0800

    Fixed IsPalindrome

--- Testing.py
+++ Testing.py
@@ -80,97 +80,118 @@ def F(n):
 print("\n\n")
 for i in range(1,10):
 	print(F(i))
+'''
+punctmylist = ['`','~','!','@','#','$','%','^','&','*','(',')','_','-','=','+','\\','|',']','}','{','}','[',"'",'"',';',':','/','?','>','<', '.',',']
 
-
-remPunct = ['`','~','!','@','#','$','%','^','&','*','(',')','_','-','=','+','\\','|',']','}','{','}','[',"'",'"',';',':','/','?','>','<','.',',']
-IsPalindrome(string):
-	while list[0] in remPunct:
-		del list[0]
-	while list[-1] in remPunct:
-		del list[-1]
-
+def IsPalindrome(string):
+    mylist = []
+    # moves items from string to mylist, removing all punctuation
+    for i in string:
+        if i not in punctmylist:
+            mylist.append(i)
+    # lowercases all letters
+    for i in range(0,len(mylist)):
+        mylist[i] = mylist[i].lower()
+    # base cases
+    if len(mylist) == 0 or len(mylist)==1:
+        return 'isPalindrome'
+    # recursion
+    # if palindrome so far, remove correct characters and repopulate string, passing it back to IsPalindrome
+    elif mylist[0]==mylist[-1]:
+        string = ''
+        del mylist[0]
+        del mylist[-1]
+        for i in mylist:
+            string += i
+        return IsPalindrome(string)
+    else:
+        return 'isNotPalindrome'
 
 
 print(IsPalindrome(input("Enter String: ")))
+
+
+
 
 '''
 
 global Solutions
 Solutions = 0
-def placeQueen(list=[[0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0]],queensLeft=8):
-#whole else statement makes list of positions a queen can go in
-if queensLeft >0:
-    listPossiblex = []
-    listPossibley = []
-    for x in range(0,8):
-        for y in range(0,8):
-        canPlace = True
-        #checks if there are any other queens in the x column for the space
-        if 1 in list[x]:
-            canPlace = False
-        # checks if there are any queens in the y column for the space
-        for x2 in range(0,8):
-            if list[x2][y]==1:
-                canPlace = False
-        # if canPlace == True here, there are no queens directly down or up or left or right
-        #check for diagonal queens
-        x3=x
-        y3=y
-        while x3<=7 and y3<=7:
-            if list[x3][y3] ==1:
-                canPlace = False
-                break
-            x3,y3 = x3+1,y3+1
-        x3 = x
-        y3 = y
-        while x3>=0 and y3>=0:
-            if list[x3][y3] ==1:
-                canPlace = False
-                break
-            x3,y3 = x3-1,y3-1
-        x3=x
-        y3=y
-
-        while x3<=7 and y3>=0:
-            if list[x3][y3] ==1:
-                canPlace = False
-                break
-            x3,y3 = x3+1,y3-1
-        x3 = x
-        y3 = y
-
-        while x3>=0 and y3<=7:
-            if list[x3][y3] ==1:
-                canPlace = False
-                break
-            x3,y3 = x3-1,y3+1
-        if canPlace == True:
-            listPossiblex.append(x)
-            listPossibley.append(y)
-    # for each possible place you can put this queen,
-    for i in range(0,len(listPossiblex)):
-        #make the possible move
-        list[listPossiblex[i]][listPossibley[i]] = 1
-        #run the function again on this templist with one move made
-        placeQueen(list,queensLeft = queensLeft-1)
-        works = True
+def placeQueen(mylist=[[0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0]],queensLeft=8):
+#whole else statement makes mylist of positions a queen can go in
+    if queensLeft >0:
+        mylistPossiblex = []
+        mylistPossibley = []
         for x in range(0,8):
-            if 1 not in list[x]:
-                works = False
-                break
-            if list[x].count(1) >1:
-                works = False
-                break
-        if works == False:
-            list[listPossiblex[i]][listPossibley[i]] = 0
-        if works == True:
+            for y in range(0,8):
+                canPlace = True
+                #checks if there are any other queens in the x column for the space
+                if 1 in mylist[x]:
+                    canPlace = False
+                # checks if there are any queens in the y column for the space
+                for x2 in range(0,8):
+                    if mylist[x2][y]==1:
+                        canPlace = False
+                # if canPlace == True here, there are no queens directly down or up or left or right
+                #check for diagonal queens
+                x3=x
+                y3=y
+                while x3<=7 and y3<=7:
+                    if mylist[x3][y3] ==1:
+                        canPlace = False
+                        break
+                    x3,y3 = x3+1,y3+1
+                x3 = x
+                y3 = y
+                while x3>=0 and y3>=0:
+                    if mylist[x3][y3] ==1:
+                        canPlace = False
+                        break
+                    x3,y3 = x3-1,y3-1
+                x3=x
+                y3=y
+
+                while x3<=7 and y3>=0:
+                    if mylist[x3][y3] ==1:
+                        canPlace = False
+                        break
+                    x3,y3 = x3+1,y3-1
+                x3 = x
+                y3 = y
+
+                while x3>=0 and y3<=7:
+                    if mylist[x3][y3] ==1:
+                        canPlace = False
+                        break
+                    x3,y3 = x3-1,y3+1
+                if canPlace == True:
+                    mylistPossiblex.append(x)
+                    mylistPossibley.append(y)
+        # for each possible place you can put this queen,
+        for i in range(0,len(mylistPossiblex)):
+            #make the possible move
+            mylist[mylistPossiblex[i]][mylistPossibley[i]] = 1
+            #run the function again on this tempmylist with one move made
+            placeQueen(mylist,queensLeft = queensLeft-1)
+            works = True
             for x in range(0,8):
-                print(list[x])
-            print()
-            global Solutions
-            Solutions += 1
+                if 1 not in mylist[x]:
+                    works = False
+                    break
+                if mylist[x].count(1) >1:
+                    works = False
+                    break
+            if works == False:
+                mylist[mylistPossiblex[i]][mylistPossibley[i]] = 0
+            if works == True:
+                for x in range(0,8):
+                    print(mylist[x])
+                print()
+                global Solutions
+                Solutions += 1
 
 
 print(placeQueen())
 print(Solutions)
 
+'''
